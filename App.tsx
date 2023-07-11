056150cadb79f26f4c2e889df80cc6779bb23ddd
voho0000 <voho0000@gmail.com>
2023-07-11 17:15:53 +0800

    add three fields

--- App.tsx
+++ App.tsx
@@ -1,14 +1,33 @@
+import React, { useState } from 'react';
+import { StyleSheet, Button, TextInput, View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Audio } from 'expo-av';
 
-export default function App() {
+const App = () => {
+  const [text, setText] = useState<string>("");
+
+  const startRecording = () => {
+    // Here goes your code for starting the recording
+  };
+
+  const sendText = () => {
+    // Here goes your code for sending the text to the server
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Start Recording" onPress={startRecording} />
+      <TextInput
+        multiline
+        style={styles.textInput}
+        onChangeText={setText}
+        value={text}
+      />
+      <Button title="Send Text" onPress={sendText} />
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +36,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    height: 200, 
+    borderColor: 'gray', 
+    borderWidth: 1,
+    marginTop: 10,
+    marginBottom: 10,
+    width: '80%',
+    padding: 5
+  },
 });
+
+export default App;
